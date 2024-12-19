b8fcb2e290f46b88e1d3abcea3b6452ac62ad058
emunsi <emunsi123@outlook.com>
2024-12-19 20:59:05 +0100

    add font family

--- dgenlibrary/src/main/java/com/example/dgenlibrary/ui/theme/Type.kt
+++ dgenlibrary/src/main/java/com/example/dgenlibrary/ui/theme/Type.kt
@@ -1,2 +1,34 @@
 package com.example.dgenlibrary.ui.theme
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.dgenlibrary.R
+
+val SourceSansProFamily = FontFamily(
+    Font(R.font., FontWeight.Light),
+    Font(R.font.firasans_regular, FontWeight.Normal),
+    Font(R.font.firasans_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.firasans_medium, FontWeight.Medium),
+    Font(R.font.firasans_bold, FontWeight.Bold)
+)
+
+val MyTypography = Typography(
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 12.sp/*...*/
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 2.sp,
+        /*...*/
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fontFamily, fontWeight = FontWeight.SemiBold/*...*/
+    ),
+    /*...*/
+)
