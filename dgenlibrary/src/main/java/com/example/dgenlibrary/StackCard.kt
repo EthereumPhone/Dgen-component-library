1b9c8b2778a46ebdb64d54e623a81a8ad73d521e
emunsi <emunsi123@outlook.com>
2025-02-20 01:00:55 +0100

    fixing scrolling

--- dgenlibrary/src/main/java/com/example/dgenlibrary/StackCard.kt
+++ dgenlibrary/src/main/java/com/example/dgenlibrary/StackCard.kt
@@ -2,6 +2,9 @@ package com.example.dgenlibrary
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -12,17 +15,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -30,12 +42,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.Layout
@@ -98,39 +112,44 @@ fun MyCardCarousel(
         listState.scrollToItem(assets.lastIndex)
     }
 
-    Box (
-        modifier = modifier.fillMaxSize().background(Color.Black).padding(horizontal = 16.dp),
-    ){
-        Box (
-            modifier = modifier.fillMaxSize().background(Color.Black).zIndex(2f),
-        ){
+//    Box (
+//        modifier = modifier.zIndex(0f).fillMaxSize().background(Color.Black).padding(horizontal = 16.dp),
+//    ){
 
-        }
         LazyColumn(
             state = listState,
             modifier = modifier.fillMaxSize().zIndex(3f),
-            verticalArrangement = Arrangement.spacedBy((-200).dp), // Overlapping effect
-            contentPadding = PaddingValues(top = 100.dp, bottom =100.dp) // Ensures enough space for scrolling
+            verticalArrangement = Arrangement.spacedBy((-225).dp), // Overlapping effect
+            contentPadding = PaddingValues(top = 60.dp, bottom =40.dp) // Ensures enough space for scrolling
         ) {
             itemsIndexed(assets) { index, item ->
                 val scrollOffset = listState.firstVisibleItemIndex + listState.firstVisibleItemScrollOffset / 1000f
                 val relativeIndex = index - scrollOffset
 
-                // Scaling and alpha logic to create the carousel effect
-                val scaleFactor = lerp(0.9f, 0.5f, (abs(relativeIndex) / assets.size).coerceIn(0f, 1f))
-                val alphaFactor = lerp(1f, 0f, relativeIndex.coerceIn(0f, 1f))
+                // Animated values
 
-                // Ensuring the front card disappears smoothly
-                val frontCardTranslation = lerp(0f, 400f, (relativeIndex).coerceIn(0f, 1f))
+                //val scaleFactor = lerp(0.9f, 0.5f, (abs(relativeIndex) / 5).coerceIn(0f, 1f))
+                val scaleFactor by animateFloatAsState(
+                    targetValue = lerp(0.8f, 0.55f, (abs(relativeIndex) / 4).coerceIn(0f, 1f)),
+                    animationSpec = tween(durationMillis = 500, easing = FastOutSlowInEasing)
+                )
 
+                val alphaFactor by animateFloatAsState(
+                    targetValue = lerp(1f, 0f, (abs(relativeIndex) / 4).coerceIn(0f, 1f)),
+                    animationSpec = tween(durationMillis = 300, easing = LinearOutSlowInEasing)
+                )
+
+                val frontCardTranslation by animateFloatAsState(
+                    targetValue = lerp(0f, 800f, (relativeIndex).coerceIn(0f, 1f)),
+                    animationSpec = tween(durationMillis = 400, easing = FastOutSlowInEasing)
+                )
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .zIndex(if (scaleFactor <= 0.51) 3f else 1f)
                         .graphicsLayer {
                             scaleX = scaleFactor
                             scaleY = scaleFactor
-                            //alpha = alphaFactor
+                            alpha = alphaFactor
                             rotationX = -5f
                             translationY = frontCardTranslation
                         },
@@ -146,8 +165,9 @@ fun MyCardCarousel(
                     },
                 )
             }
+//
         }
-    }
+//    }
 
 }
 
@@ -239,10 +259,70 @@ fun MyCardColumnPreview() {
 
         )
 
-    MyCardCarousel(
-        modifier = Modifier,
-        assets = list
-    )
+
+    Column (
+        modifier = Modifier.fillMaxSize().background(Color.Black),
+
+        ) {
+
+        Box(
+            modifier = Modifier
+                .weight(1f) // Allows it to take up remaining space
+                .padding(horizontal = 16.dp)
+        ) {
+            MyCardCarousel(
+                modifier = Modifier,
+                assets = list
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+
+                .height(70.dp) // Adjust height as needed
+        ) {
+            // Fading border overlay (Green to Transparent)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(16.dp) // Adjust thickness of fading border
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent,Color.Black)
+                        )
+                    )
+
+            )
+
+            // Centered Row of Buttons (Below Fading Border)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                     // Prevents it from being too close to the border
+                .background(Color.Black).padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                IconButton(onClick = { /* TODO: Action */ }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.width(32.dp))
+                IconButton(onClick = { /* TODO: Action */ }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = "Forward",
+                        tint = Color.White
+                    )
+                }
+            }
+        }
+    }
+
 }
 
 
