e9e1b8357ec6dd41145d328eaf02de26e61506ed
emunsi <emunsi123@outlook.com>
2025-02-20 15:36:17 +0100

    CardColumnPreview update

--- dgenlibrary/src/main/java/com/example/dgenlibrary/StackCard.kt
+++ dgenlibrary/src/main/java/com/example/dgenlibrary/StackCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,6 +24,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -33,6 +35,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -54,10 +58,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.fontscaling.MathUtils.lerp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import com.example.dgenlibrary.ui.theme.PitagonsSans
+import com.example.dgenlibrary.ui.theme.SpaceMono
+import com.example.dgenlibrary.ui.theme.dgenBlack
+import com.example.dgenlibrary.ui.theme.dgenTurqoise
+import com.example.dgenlibrary.ui.theme.dgenWhite
 import kotlin.math.abs
 
 
@@ -119,8 +133,8 @@ fun MyCardCarousel(
         LazyColumn(
             state = listState,
             modifier = modifier.fillMaxSize().zIndex(3f),
-            verticalArrangement = Arrangement.spacedBy((-225).dp), // Overlapping effect
-            contentPadding = PaddingValues(top = 60.dp, bottom =40.dp) // Ensures enough space for scrolling
+            verticalArrangement = Arrangement.spacedBy((-240).dp), // Overlapping effect
+            contentPadding = PaddingValues(top = 50.dp, bottom =50.dp) // Ensures enough space for scrolling
         ) {
             itemsIndexed(assets) { index, item ->
                 val scrollOffset = listState.firstVisibleItemIndex + listState.firstVisibleItemScrollOffset / 1000f
@@ -150,7 +164,7 @@ fun MyCardCarousel(
                             scaleX = scaleFactor
                             scaleY = scaleFactor
                             alpha = alphaFactor
-                            rotationX = -5f
+                            rotationX = -10f
                             translationY = frontCardTranslation
                         },
                     backgroundColor = item.backgroundColor,
@@ -260,67 +274,274 @@ fun MyCardColumnPreview() {
         )
 
 
-    Column (
+    Box (
         modifier = Modifier.fillMaxSize().background(Color.Black),
-
-        ) {
+    ) {
 
         Box(
             modifier = Modifier
-                .weight(1f) // Allows it to take up remaining space
-                .padding(horizontal = 16.dp)
+                .align(Alignment.TopCenter)
+                .fillMaxSize()
+                .background(dgenBlack)
+                //.weight(1f) // Allows it to take up remaining space
+
         ) {
             MyCardCarousel(
-                modifier = Modifier,
+                modifier = Modifier.padding(bottom = 16.dp),
                 assets = list
             )
         }
-
         Column(
             modifier = Modifier
+                .align(Alignment.BottomCenter)
                 .fillMaxWidth()
 
-                .height(70.dp) // Adjust height as needed
         ) {
             // Fading border overlay (Green to Transparent)
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(16.dp) // Adjust thickness of fading border
+                    .height(40.dp) // Adjust thickness of fading border
                     .background(
                         brush = Brush.verticalGradient(
-                            colors = listOf(Color.Transparent,Color.Black)
+                            colors = listOf(Color.Transparent, dgenBlack)
                         )
                     )
 
             )
-
-            // Centered Row of Buttons (Below Fading Border)
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(1f)
-                     // Prevents it from being too close to the border
-                .background(Color.Black).padding(bottom = 8.dp),
+                    .background(dgenBlack)
+                    .padding(8.dp),
                 horizontalArrangement = Arrangement.Center
             ) {
-                IconButton(onClick = { /* TODO: Action */ }) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back",
-                        tint = Color.White
-                    )
+                IconButton(modifier = Modifier, onClick = { /* TODO: Action */ }) {
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier = Modifier.width(16.dp),
+                            painter = painterResource(R.drawable.baseline_arrow_outward_24),
+                            contentDescription = "Back",
+                            tint = dgenTurqoise
+                        )
+                        Text(
+                            text= "SEND",
+                            style = TextStyle(
+                                fontFamily = SpaceMono,
+                                color = dgenTurqoise,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 12.sp,
+                                lineHeight = 12.sp,
+                                letterSpacing = 0.sp,
+                                textDecoration = TextDecoration.None
+                            )
+                        )
+                    }
+
                 }
                 Spacer(modifier = Modifier.width(32.dp))
-                IconButton(onClick = { /* TODO: Action */ }) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowForward,
-                        contentDescription = "Forward",
-                        tint = Color.White
-                    )
+                IconButton(modifier = Modifier, onClick = { /* TODO: Action */ }) {
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier = Modifier.width(16.dp).graphicsLayer{
+                                rotationZ = 90f
+                            },
+                            painter = painterResource(R.drawable.baseline_swap_horiz_24),
+                            contentDescription = "Back",
+                            tint = dgenTurqoise
+                        )
+                        Text(
+                            text= "LOG",
+                            style = TextStyle(
+                                fontFamily = SpaceMono,
+                                color = dgenTurqoise,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 12.sp,
+                                lineHeight = 12.sp,
+                                letterSpacing = 0.sp,
+                                textDecoration = TextDecoration.None
+                            )
+                        )
+                    }
+
                 }
+                Spacer(modifier = Modifier.width(32.dp))
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = dgenTurqoise,
+                        disabledContainerColor = Color.Transparent,
+                        disabledContentColor = Color.Gray
+                    ),
+                    contentPadding = PaddingValues(0.dp),
+                    modifier = Modifier.width(IntrinsicSize.Max),
+                    onClick = { /* TODO: Action */ }
+                ) {
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier = Modifier.width(16.dp).graphicsLayer{
+                                rotationZ = 90f
+                            },
+                            painter = painterResource(R.drawable.qr_code),
+                            contentDescription = "Back",
+                            tint = dgenTurqoise
+                        )
+                        Text(
+                            text= "CPY ADD",
+                            style = TextStyle(
+                                fontFamily = SpaceMono,
+                                color = dgenTurqoise,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 12.sp,
+                                lineHeight = 12.sp,
+                                letterSpacing = 0.sp,
+                                textDecoration = TextDecoration.None
+                            )
+                        )
+                    }
+
+                }
+
             }
         }
+
+
+
+//        Column(
+//            modifier = Modifier
+//                .align(Alignment.BottomCenter)
+//                .fillMaxWidth()
+//
+//                .height(IntrinsicSize.Max) // Adjust height as needed
+//        ) {
+//            // Fading border overlay (Green to Transparent)
+//            Box(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(16.dp) // Adjust thickness of fading border
+//                    .background(
+//                        brush = Brush.verticalGradient(
+//                            colors = listOf(Color.Transparent,Color.Black)
+//                        )
+//                    )
+//
+//            )
+//
+//            // Centered Row of Buttons (Below Fading Border)
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//
+//                    // Prevents it from being too close to the border
+//                    .background(Color.Black),
+//                horizontalArrangement = Arrangement.Center
+//            ) {
+//                IconButton(modifier = Modifier.background(Color.Red), onClick = { /* TODO: Action */ }) {
+//
+//                    Column(
+//                        horizontalAlignment = Alignment.CenterHorizontally
+//                    ) {
+//                        Icon(
+//                            modifier = Modifier.width(16.dp),
+//                            painter = painterResource(R.drawable.baseline_arrow_outward_24),
+//                            contentDescription = "Back",
+//                            tint = Color.White
+//                        )
+//                        Text(
+//                            text= "SEND",
+//                            style = TextStyle(
+//                                fontFamily = SpaceMono,
+//                                color = dgenWhite,
+//                                fontWeight = FontWeight.SemiBold,
+//                                fontSize = 12.sp,
+//                                lineHeight = 12.sp,
+//                                letterSpacing = 0.sp,
+//                                textDecoration = TextDecoration.None
+//                            )
+//                        )
+//                    }
+//
+//                }
+//                Spacer(modifier = Modifier.width(32.dp))
+//                IconButton(modifier = Modifier.background(Color.Red), onClick = { /* TODO: Action */ }) {
+//
+//                    Column(
+//                        horizontalAlignment = Alignment.CenterHorizontally
+//                    ) {
+//                        Icon(
+//                            modifier = Modifier.width(16.dp).graphicsLayer{
+//                                rotationZ = 90f
+//                            },
+//                            painter = painterResource(R.drawable.baseline_swap_horiz_24),
+//                            contentDescription = "Back",
+//                            tint = Color.White
+//                        )
+//                        Text(
+//                            text= "LOG",
+//                            style = TextStyle(
+//                                fontFamily = SpaceMono,
+//                                color = dgenWhite,
+//                                fontWeight = FontWeight.SemiBold,
+//                                fontSize = 12.sp,
+//                                lineHeight = 12.sp,
+//                                letterSpacing = 0.sp,
+//                                textDecoration = TextDecoration.None
+//                            )
+//                        )
+//                    }
+//
+//                }
+//                Spacer(modifier = Modifier.width(32.dp))
+//                Button(
+//                    colors = ButtonDefaults.buttonColors(
+//                        containerColor = Color.Transparent,
+//                        contentColor = Color.White,
+//                        disabledContainerColor = Color.Transparent,
+//                        disabledContentColor = Color.Gray
+//                    ),
+//                    contentPadding = PaddingValues(0.dp),
+//                    modifier = Modifier.width(IntrinsicSize.Max).background(Color.Red),
+//                    onClick = { /* TODO: Action */ }
+//                ) {
+//
+//                    Column(
+//                        horizontalAlignment = Alignment.CenterHorizontally
+//                    ) {
+//                        Icon(
+//                            modifier = Modifier.width(16.dp).graphicsLayer{
+//                                rotationZ = 90f
+//                            },
+//                            painter = painterResource(R.drawable.qr_code),
+//                            contentDescription = "Back",
+//                            tint = Color.White
+//                        )
+//                        Text(
+//                            text= "CPY ADD",
+//                            style = TextStyle(
+//                                fontFamily = SpaceMono,
+//                                color = dgenWhite,
+//                                fontWeight = FontWeight.SemiBold,
+//                                fontSize = 12.sp,
+//                                lineHeight = 12.sp,
+//                                letterSpacing = 0.sp,
+//                                textDecoration = TextDecoration.None
+//                            )
+//                        )
+//                    }
+//
+//                }
+//
+//            }
+//        }
     }
 
 }
