19fdbe77d9fe1b462790cf811c8748a499c4559d
emunsi <emunsi123@outlook.com>
2026-02-04 13:49:57 +0100

    add: CircularIconButton

--- dgenlibrary/src/main/java/com/example/dgenlibrary/calculator/CircularButton.kt
+++ dgenlibrary/src/main/java/com/example/dgenlibrary/calculator/CircularButton.kt
@@ -2,30 +2,42 @@ package com.example.dgenlibrary.calculator
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dgenlibrary.R
+import com.example.dgenlibrary.SystemColorManager.primaryColor
+import com.example.dgenlibrary.SystemColorManager.secondaryColor
 import com.example.dgenlibrary.ui.theme.IconButtonSize
 import com.example.dgenlibrary.ui.theme.PitagonsSans
+import com.example.dgenlibrary.ui.theme.dgenBlack
 import com.example.dgenlibrary.ui.theme.dgenTurqoise
 
 @Composable
@@ -37,10 +49,11 @@ fun CircularButton(
     text: String,
     textColor: Color = dgenTurqoise,
     borderColor: Color = Color.Transparent,
-    fontFamily: FontFamily = PitagonsSans,
+    fontFamily: FontFamily = FontFamily(Font(R.font.pitagonsanstext_semibold)),
     containerColor: Color,
     onClick: () -> Unit,
 ) {
+
     val hapticFeedback = LocalHapticFeedback.current
 
     Button(
@@ -62,21 +75,187 @@ fun CircularButton(
             )
             .border(2.dp, borderColor, CircleShape)
     ) {
-        Column(
+        Box(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            Alignment.Center
         ) {
             Text(
                 text = text,
                 style = TextStyle(
                     fontFamily = fontFamily,
                     fontSize = 30.sp,
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.SemiBold
+                    textAlign = TextAlign.Center
                 ),
                 color = textColor)
         }
 
+    }
+}
+
+@Composable
+fun CircularIconButton(
+    modifier: Modifier = Modifier,
+    size: Dp = IconButtonSize,
+    icon: @Composable () -> Unit,
+    borderColor: Color = Color.Transparent,
+    containerColor: Color,
+    onClick: () -> Unit,
+) {
+
+    val hapticFeedback = LocalHapticFeedback.current
+
+    IconButton(
+        onClick = {
+            hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+            onClick()
+        },
+        colors = IconButtonDefaults.filledIconButtonColors(
+            containerColor = containerColor
+        ),
+
+        modifier = modifier.size(size = size).clip(CircleShape).border(2.dp, borderColor, CircleShape)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            Alignment.Center
+        ) {
+            icon()
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun CircularButtonPreview(){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    )
+    {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ){
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.sign),
+                        contentDescription = "QR-Code",
+                        tint = primaryColor
+                    )
+                },
+                containerColor = secondaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.percent),
+                        contentDescription = "QR-Code",
+                        tint = primaryColor
+                    )
+                },
+                containerColor = secondaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Absolute.spacedBy(16.dp),
+        ){
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.multiply),
+                        contentDescription = "QR-Code",
+                        tint = primaryColor
+                    )
+                },
+                containerColor = secondaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.divide),
+                        contentDescription = "QR-Code",
+                        tint = primaryColor
+                    )
+                },
+                containerColor = secondaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Absolute.spacedBy(16.dp),
+        ){
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.add),
+                        contentDescription = "QR-Code",
+                        tint = primaryColor
+                    )
+                },
+                containerColor = secondaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.minus),
+                        contentDescription = "QR-Code",
+                        tint = primaryColor
+                    )
+                },
+                containerColor = secondaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Absolute.spacedBy(16.dp),
+        ){
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(40.dp),
+                        painter = painterResource(R.drawable.cancel),
+                        contentDescription = "QR-Code",
+                        tint = secondaryColor
+                    )
+                },
+                containerColor = primaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+            CircularIconButton(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(R.drawable.equal),
+                        contentDescription = "QR-Code",
+                        tint = secondaryColor
+                    )
+                },
+                containerColor = primaryColor,
+                borderColor = primaryColor,
+                onClick = {  }
+            )
+        }
     }
 }
